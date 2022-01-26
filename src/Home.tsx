3b4b952ac3c3b15edbb512fb9fb4dcac87f0a83a
Celestriall <wissman69@hotmail.com>
2022-01-26 17:45:07 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -603,7 +603,7 @@ const Home = (props: HomeProps) => {
                   View on Solana Explorer
                 </SolExplorerLink>
               )}
-           <img /> </NFT>
+              </img> </NFT>
           </DesContainer>
           {/* <DesContainer>
             <Des elevation={2}>
