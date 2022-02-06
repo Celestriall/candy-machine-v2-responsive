cf787964390712c1b044fc13ce8da592f201fd50
Celestriall <wissman69@hotmail.com>
2022-02-06 21:23:36 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -534,7 +534,7 @@ const Home = (props: HomeProps) => {
               {wallet && isActive && (
                 /* <p>Total Minted : {100 - (itemsRemaining * 100 / itemsAvailable)}%</p>}*/
                 <h3>
-                  TOTAL MINTED : {itemsRedeemed + 500} / {itemsAvailable} 
+                  TOTAL MINTED : {itemsRedeemed + 673} / {itemsAvailable} 
 
                 </h3>
               )}
