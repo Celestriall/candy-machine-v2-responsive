9886a23c5d38cb95413216ab3292234572e77d73
Celestriall <wissman69@hotmail.com>
2022-02-07 17:39:31 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -534,14 +534,14 @@ const Home = (props: HomeProps) => {
               {wallet && isActive && (
                 /* <p>Total Minted : {100 - (itemsRemaining * 100 / itemsAvailable)}%</p>}*/
                 <h3>
-                  TOTAL MINTED : {4178} / {itemsAvailable} 
+                  TOTAL MINTED : {itemsRedeemed} / {itemsAvailable} 
 
                 </h3>
               )}
               {wallet && isActive && (
                 <BorderLinearProgress
                   variant="determinate"
-                   value={100 - ((4178) * 100) / itemsAvailable}
+                   value={100 - (itemsRemaining * 100) / itemsAvailable}
                   //value={100 - ((800 - itemsRedeemed) * 100) / 800}
                 />
               )}
